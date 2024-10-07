5773825519e684099aa422475632dfb32c1386eb
margo <margo171190@mail.ru>
2024-10-07 14:19:41 +0400

    removed unused code

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,19 +1,12 @@
 package web.controller;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 @Controller
-@ComponentScan("web")
 public class CarsController {
 
     private final CarService carService;
